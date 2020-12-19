a03069f5ce4f395219741eb2126acaddd9c5a06a
preeti999 <preetik.ce1998@gmail.com>
2020-12-18 17:23:32 -0700

    Add note button build successfully

---
+++ js/note.js
@@ -0,0 +1,20 @@
+console.log("Start saving your notes");
+
+//Add a note to local storage
+let addBtn = document.getElementById('addBtn');
+addBtn.addEventListener("click", function (e) {
+
+    let addTxt = document.getElementById("addTxt");
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+    }
+    else {
+        notesObj = JSON.parse(notes);
+
+    }
+    notesObj.push(addTxt.value);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    addTxt.value = "";
+    console.log(notesObj);
+})
