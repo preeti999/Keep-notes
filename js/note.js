89386d7988485b8c715edf52277d82d251cc7e91
preeti999 <preetik.ce1998@gmail.com>
2020-12-18 23:49:02 -0700

    added styling and popup

--- js/note.js
+++ js/note.js
@@ -5,38 +5,45 @@ showNotes();
 //Add a note to local storage
 let addBtn = document.getElementById('addBtn');
 addBtn.addEventListener("click", function (e) {
-    
-    let addTxt = document.getElementById("addTxt");
-    let notes = localStorage.getItem("notes");
-    if (notes == null) {
-        notesObj = [];
+    let txt = addTxt.value;
+    if (txt == "") {
+        window.alert("Please write some note before saving")
     }
     else {
-        notesObj = JSON.parse(notes);
 
-    }
-    
-    notesObj.push(addTxt.value);
-    localStorage.setItem("notes", JSON.stringify(notesObj));
-    addTxt.value = "";
-    console.log(notesObj);  
-    showNotes();
-})
-
-    //Function to show all notes available to local storage
-    function showNotes() {
+        let addTxt = document.getElementById("addTxt");
         let notes = localStorage.getItem("notes");
         if (notes == null) {
             notesObj = [];
         }
         else {
             notesObj = JSON.parse(notes);
-        }
 
-        //Create a note section
-        let html = "";
-        notesObj.forEach(function (element, index) {
-            html += `
+        }
+    
+
+    notesObj.push(addTxt.value);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    addTxt.value = "";
+    console.log(notesObj);
+    showNotes();
+    }
+})
+
+//Function to show all notes available to local storage
+function showNotes() {
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+    }
+    else {
+        notesObj = JSON.parse(notes);
+    }
+
+    //Create a note section
+    let html = "";
+    notesObj.forEach(function (element, index) {
+        html += `
                 <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
                     <div class="card-body">
                         <h5 class="card-title">Note ${index + 1}</h5>
@@ -47,48 +54,48 @@ addBtn.addEventListener("click", function (e) {
                     </div>
                 </div>`;
 
-        });
-        let notesElm = document.getElementById("notes");
-        if (notesObj.length != 0) {
-            notesElm.innerHTML = html;
-        }
-        else {
-            notesElm.innerHTML = `Nothing to show!! Please add a NOTE`
-        }
+    });
+    let notesElm = document.getElementById("notes");
+    if (notesObj.length != 0) {
+        notesElm.innerHTML = html;
+    }
+    else {
+        notesElm.innerHTML = `Nothing to show!! Please add some NOTE`
+    }
+}
+
+//Function to delete a note
+function deleteNote(index) {
+    console.log("I am delete a note", index);
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+    }
+    else {
+        notesObj = JSON.parse(notes);
     }
 
-    //Function to delete a note
-    function deleteNote(index) {
-        console.log("I am delete a note", index);
-        let notes = localStorage.getItem("notes");
-        if (notes == null) {
-            notesObj = [];
+    notesObj.splice(index, 1);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    showNotes();
+
+}
+
+//Search keyword from the text feild
+let search = document.getElementById('searchTxt');
+search.addEventListener("input", function () {
+
+    let inputVal = search.value.toLowerCase();
+    // console.log('Input event fired!', inputVal);
+    let noteCards = document.getElementsByClassName('noteCard');
+    Array.from(noteCards).forEach(function (element) {
+        let cardTxt = element.getElementsByTagName("p")[0].innerText;
+        if (cardTxt.includes(inputVal)) {
+            element.style.display = "block";
         }
         else {
-            notesObj = JSON.parse(notes);
+            element.style.display = "none";
         }
-
-        notesObj.splice(index, 1);
-        localStorage.setItem("notes", JSON.stringify(notesObj));
-        showNotes();
-
-    }
-
-    //Search keyword from the text feild
-    let search = document.getElementById('searchTxt');
-    search.addEventListener("input", function () {
-
-        let inputVal = search.value.toLowerCase();
-        // console.log('Input event fired!', inputVal);
-        let noteCards = document.getElementsByClassName('noteCard');
-        Array.from(noteCards).forEach(function (element) {
-            let cardTxt = element.getElementsByTagName("p")[0].innerText;
-            if (cardTxt.includes(inputVal)) {
-                element.style.display = "block";
-            }
-            else {
-                element.style.display = "none";
-            }
-            // console.log(cardTxt);
-        })
+        // console.log(cardTxt);
     })
+})
